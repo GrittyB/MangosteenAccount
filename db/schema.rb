e5f78b182846d9566240319c2e04096e374f87a3
grittyb <maat_ol@outlook.com>
2023-07-18 18:59:17 +0800

    create_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_144135) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_103721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "amount"
+    t.text "notes"
+    t.bigint "tags_id", array: true
+    t.datetime "happen_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "validation_codes", force: :cascade do |t|
     t.string "email"
